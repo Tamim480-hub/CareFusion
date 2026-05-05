8aef1d205d19568a089f3ab29c7eef0abaf02687
Rumana05 <rumana8230421@gmail.com>
2026-05-06 00:04:39 +0600

    Sprint 1

--- myapp/templatetags/hospital.py
+++ myapp/templatetags/hospital.py
@@ -14,3 +14,10 @@ class Doctor(models.Model):
 
     def __str__(self):
         return self.name
+class Medicine(models.Model):
+    name = models.CharField(max_length=200)
+    company = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
