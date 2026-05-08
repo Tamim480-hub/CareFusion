e8e2fc9a0a4753eb541c907bddd9296e0034fd9d
Rumana05 <rumana8230421@gmail.com>
2026-05-08 23:39:35 +0600

    Sprint1

--- myapp1/hospital.py
+++ myapp1/hospital.py
@@ -7,3 +7,22 @@ def search_doctor(request):
     query = request.GET.get('q')
 
     doctors = Doctor.objects.all()
+if query:
+        doctors = doctors.filter(
+            specialization__icontains=query
+        ) | doctors.filter(
+            user__first_name__icontains=query
+        ) | doctors.filter(
+            user__last_name__icontains=query
+        )
+
+    context = {
+        'doctors': doctors,
+        'query': query
+    }
+
+    return render(
+        request,
+        'search_doctor.html',
+        context
+    )
